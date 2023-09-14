d1c5e05514ab00ffb1379f066ed972ae53a6c9db
jaejungbyun <dogmnil2007@naver.com>
2023-09-14 10:36:55 +0900

    ss

--- src/app/components/catagory_component/Detail_btn.tsx
+++ src/app/components/catagory_component/Detail_btn.tsx
@@ -1,12 +1,12 @@
-import <style></style> from '@/app/styles/RoomCata.module.scss'
+import style from '@/app/styles/RoomCata.module.scss'
 
 export default function Detail_btn() {
     return (
         <>
-            <div className={<style></style>.detail_div}>
-                <h3 className={<style></style>.detail_cata_word}>상세 조건</h3>
+            <div className={style.detail_div}>
+                <h3 className={style.detail_cata_word}>상세 조건</h3>
             </div>
-            <div className={<style></style>.btn_wrap}>
+            <div className={style.btn_wrap}>
                 <button>초기화</button>
                 <button>적용</button>
             </div>
