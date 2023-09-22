bf16e750e6d4928be99d33c56605b7f998d07f39
jyj <busy.joj@gmail.com>
2023-09-22 17:27:41 +0900

    Modify 지역 selectBox

--- src/app/components/select/SelectBox.tsx
+++ src/app/components/select/SelectBox.tsx
@@ -5,7 +5,7 @@ import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown'
 import style from '@/app/components/select/select.module.scss'
 
 const SelectBox = () => {
-    const [currentValue, setCurrentValue] = useState('서울')
+    const [currentValue, setCurrentValue] = useState('전체')
     const optionShowRef = useRef(null)
 
     const handleChangeSelect = (e) => {
@@ -18,24 +18,29 @@ const SelectBox = () => {
             liAll[i].classList.remove(style['on'])
         }
         e.target.parentElement.classList.add(style['on'])
+        optionShowRef.current.classList.remove(style['on'])
     }
     return (
         <div className={style.select}>
             <label
                 className={style.selectLabel}
-                onMouseOver={() => {
-                    optionShowRef.current.classList.add(style['on'])
+                onClick={() => {
+                    if (optionShowRef.current.classList.contains(style['on'])) {
+                        optionShowRef.current.classList.remove(style['on'])
+                    } else {
+                        optionShowRef.current.classList.add(style['on'])
+                    }
                 }}>
                 {currentValue}
                 <ArrowDropDownIcon className={style.icon} />
             </label>
-            <ul
-                className={style.selectOption}
-                ref={optionShowRef}
-                onMouseLeave={() => {
-                    optionShowRef.current.classList.remove(style['on'])
-                }}>
+            <ul className={style.selectOption} ref={optionShowRef}>
                 <li className={`${style.item} ${style.on}`}>
+                    <Link href="/" onClick={handleChangeSelect}>
+                        전체
+                    </Link>
+                </li>
+                <li className={style.item}>
                     <Link href="/" onClick={handleChangeSelect}>
                         서울
                     </Link>
