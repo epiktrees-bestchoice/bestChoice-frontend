4166455f82f5a1c3cad1c654eb16ead6cfcd871e
jyj <busy.joj@gmail.com>
2023-09-24 22:08:50 +0900

    add RoomList context APi

--- src/app/provider/providers.tsx
+++ src/app/provider/providers.tsx
@@ -1,11 +1,14 @@
 'use client'
 import IsLoginProvider from '@/app/provider/IsLoginProvider'
+import RoomListContext from '@/app/provider/roomListProvider'
 import { StyledEngineProvider } from '@mui/material'
 
 export default function Providers({ children }) {
     return (
         <StyledEngineProvider injectFirst>
-            <IsLoginProvider>{children}</IsLoginProvider>
+            <IsLoginProvider>
+                <RoomListContext>{children}</RoomListContext>
+            </IsLoginProvider>
         </StyledEngineProvider>
     )
 }
