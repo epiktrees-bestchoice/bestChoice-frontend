4a2c10a81525c82a20315324dc1b923fe7edb337
jyj <busy.joj@gmail.com>
2023-09-26 16:03:00 +0900

    Modify room App route 수정

--- src/app/api/room/route.ts
+++ src/app/api/room/route.ts
@@ -1,9 +1,12 @@
 import { NextRequest, NextResponse } from 'next/server'
 export async function GET(request: NextRequest) {
-    const res = await fetch(
-        'https://api.epicktrees.net/api/product/accommodation/all',
-    )
-
-    const data = await res.json()
-    return NextResponse.json({ data: data }, { status: 200 })
+    try {
+        const res = await fetch(
+            'https://api.epicktrees.net/api/product/accommodation/all',
+        )
+        const data = await res.json()
+        return NextResponse.json({ data: data }, { status: 200 })
+    } catch (error) {
+        return NextResponse.json({ error: error.message }, { status: 500 })
+    }
 }
