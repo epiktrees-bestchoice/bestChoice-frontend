3bbcd495dacfc70c0847d036a0484d4edae8fdbc
jyj <busy.joj@gmail.com>
2023-09-27 01:39:17 +0900

    fix vercel 배포 에러

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,13 +1,11 @@
 import { NextRequest, NextResponse } from 'next/server'
 
-export const cookieValue = 'JSESSIONID=34668C266465637EDC65985A011AE00F'
-
 export async function GET(request: NextRequest) {
     try {
         const res = await fetch('https://api.epicktrees.net/api/v1/my/info', {
             method: 'GET',
             headers: {
-                cookie: cookieValue,
+                cookie: 'JSESSIONID=34668C266465637EDC65985A011AE00F',
             },
             credentials: 'include',
         })
